83b9d9fa2b51c5ee0989b5bfc40937ef69a8c66f
Moji Norouzi zadeh <mnz1988@aol.com>
2024-10-29 22:15:03 +0330

    gallery description color changes

--- dist/assets/ios.transition-legacy-CXk_QMFs.js
+++ dist/assets/ios.transition-legacy-CXk_QMFs.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DgfnBUuT.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-DgD6yzKF.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
